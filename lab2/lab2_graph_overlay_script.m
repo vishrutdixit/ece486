6743109f1e1b9ea21f7bc5e1eca8a2ca1e547281
Gupta <kg3@illinois.edu>
2017-09-27 13:56:43 -0500

    lab3 simulink

---
+++ lab2/lab2_graph_overlay_script.m
@@ -0,0 +1,10 @@
+cd U:\ece486\
+
+hold on
+grid on
+plot(tout3a, y3a, '-')
+plot(tout3b, y3b, ':')
+plot(tout3c, y3c, '--')
+title('y vs. t for extra pole systems')
+xlabel('t, time (s)')
+ylabel('y')
