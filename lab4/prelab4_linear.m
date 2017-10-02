65babc792f0f8eecb6ec90d49530a79ea3f361c8
Vishrut Dixit <vishrutdixit@gmail.com>
2017-10-02 02:57:35 -0500

    units

--- lab4/prelab4_linear.m
+++ lab4/prelab4_linear.m
@@ -5,13 +5,10 @@ close all
 time = Elect(:,1);
 voltage = Elect(:,2);
 
-voltage = voltage/1000;
-
 Rs = 2.5;
 Ra = 3.3;
 
-Vss = 0.425/1000;
-%Vss = (Rs*.001)/(Rs+Ra)
+Vss = 0.430;
 
 out = log(Vss - voltage);
 
@@ -30,6 +27,5 @@ ylabel('$ln(V_{ss} - V_{0}(t))$', 'interpreter', 'latex');
 title('Time vs. Voltage Linear Fit');
 legend('actual', 'linear fit');
 
-La = (Vss*(Rs+Ra))/Rs
-Te = La/(Ra+Rs)
-
+Te = -1/coeffs(1)
+La = Te*(Ra+Rs)
