4fee0eba570a528dd015c62ae9330dd15e82c87e
Kush Gupta <kg3@linux-a2.ews.illinois.edu>
2017-10-03 16:54:40 -0500

    working

--- SpecGraphGenerator.m
+++ SpecGraphGenerator.m
@@ -1,6 +1,6 @@
-[Mp_1, tr_1, ts_1] = StepResponseMetrics(y3a,tout3a, 1, 1)
-[Mp_2, tr_2, ts_2] = StepResponseMetrics(y3b,tout3b, 1, 1);
-[Mp_3, tr_3, ts_3] = StepResponseMetrics(y3c,tout3c, 1, 1);
+[Mp_1, tr_1, ts_1] = StepResponseMetrics(ctrl1_wr_w,ctrl1_wr_t, 1, 1)
+[Mp_2, tr_2, ts_2] = StepResponseMetrics(ctrl2_wr_w,ctrl2_wr_t, 1, 1);
+[Mp_3, tr_3, ts_3] = StepResponseMetrics(ctrl3_wr_w,ctrl3_wr_t, 1, 1);
 %[Mp_4, tr_4, ts_4] = StepResponseMetrics(y2d,tout2d, 1, 1);
 %[Mp_5, tr_5, ts_5] = StepResponseMetrics(y2e,tout2e, 1, 1);
 
