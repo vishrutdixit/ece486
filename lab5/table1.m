1b7fa028ce8f894dcff10434cb94e983827dd5e9
Vishrut Dixit <vjdixit2@gelib-4c-39.ews.illinois.edu>
2017-10-31 18:51:46 -0500

    table1 theoretical calculations

---
+++ lab5/table1.m
@@ -0,0 +1,23 @@
+P1 = .888;
+P2 = .807;
+
+w = sqrt(3456*P1);
+zeta = (64.8*P2 + 5)/(2*w)
+w
+Mp = exp((-pi*zeta)/(sqrt(1-zeta^2)));
+Mp = Mp*100
+
+if(zeta > 0 && zeta < 1.2)
+    tr = (1.2 - 0.45*zeta + 2.6*(zeta^2))/(w)
+else
+    tr = (4.7*zeta - 1.2)/w;
+end
+
+if(zeta > 0 && zeta <= 0.69)
+    ts = (-0.5/(zeta*w))*log((1-zeta^2)/400)
+else
+    ts = (6.6*zeta - 1.6)/w
+end
+
+error = 1/w^2
+
