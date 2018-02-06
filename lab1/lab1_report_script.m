5eef20e44488a2af7e6180648a5f5ae2207012a4
Vishrut Dixit <vishrutdixit@gmail.com>
2018-02-05 18:26:55 -0600

    Update lab1_report_script.m

--- lab1/lab1_report_script.m
+++ lab1/lab1_report_script.m
@@ -1,8 +1,8 @@
-[Mp_1, tr_1, ts_1] = StepResponseMetrics(y2a,tout2a, 1, 1)
-[Mp, tr, ts] = StepResponseMetrics(y(:,1),tout, 1, 1);
-[Mp_3, tr_3, ts_3] = StepResponseMetrics(y2c,tout2c, 1, 1);
-[Mp_4, tr_4, ts_4] = StepResponseMetrics(y2d,tout2d, 1, 1);
-[Mp_5, tr_5, ts_5] = StepResponseMetrics(y2e,tout2e, 1, 1);
+[Mp_1, tr_1, ts_1] = StepResponseMetrics(y2a,tout2a, 1, 2)
+[Mp, tr, ts] = StepResponseMetrics(y(:,1),tout, 1, 2);
+[Mp_3, tr_3, ts_3] = StepResponseMetrics(y2c,tout2c, 1, 2);
+[Mp_4, tr_4, ts_4] = StepResponseMetrics(y2d,tout2d, 1, 2);
+[Mp_5, tr_5, ts_5] = StepResponseMetrics(y2e,tout2e, 1, 2);
 [Mp_6, tr_6, ts_6] = StepResponseMetrics(y6(:,2),y6(:,1), 1, 2);
 [Mp_7, tr_7, ts_7] = StepResponseMetrics(y7(:,2),y7(:,1), 1, 2);
 [Mp_8, tr_8, ts_8] = StepResponseMetrics(y8(:,2),y8(:,1), 1, 2);
